cdfcb39571e28639e5b321f943e1d8038f436872
jamarsanj <jaime.due@gmaial.com>
2023-03-29 23:21:56 +0100

    para subir

---
+++ Ejercicio_Grafo/ets_grafos_jaimemartinez.java
@@ -0,0 +1,18 @@
+//esto es solo el codigo del ejercicio 1//
+public void aMethod() {
+    if (a&&b){
+        doSomething();
+    } else if (c) {
+        doSomethingElse();
+    }
+}
+//**************************//
+//esto es solo el codigo del ejercicio 2//
+public void aMethod() {
+    if (a||b){
+        doSomething();
+    } else if (c) {
+        doSomethingElse();
+    }
+}
+}
